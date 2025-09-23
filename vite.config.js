ad1b33cfc17334c83322b4f9ed9a7d3b41b9f7dc
frantan <119488618+overuseofrem@users.noreply.github.com>
2025-09-23 23:09:55 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/overuseofrem/monogram-shop-recreate', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+})
